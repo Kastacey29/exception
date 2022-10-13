9150cf0d45c67310904b75b2787c513f7fdd3b2e
Stacey29 <Kastacey@yandex.ru>
2022-10-13 20:17:31 +0300

    первое задание выполнено

--- src/Data.java
+++ src/Data.java
@@ -1,61 +1,42 @@
 public class Data {
+private final static String EXAMPLE="abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
 
-    private String login;
-    private String password;
-    private String confirmPassword;
 
-    public Data(String login, String password, String confirmPassword) {
-        this.login = login;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
+    private Data() {
+
     }
 
-    public static boolean checkData(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        if (checkSymbols(login) != true) {
+    public static boolean check(String login, String password, String confirmPassword) {
+        try {
+            checkData(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return  false;
+        }return true;
+    }
+
+    private static void checkData(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+
+        if (!checkSymbols(login)) {
             throw new WrongLoginException();
         }
-        if (checkSymbols(password) != true || confirmPassword.equals(password) == false) {
+        if (!checkSymbols(password) || !confirmPassword.equals(password)) {
             throw new WrongPasswordException();
-        } else return true;
+        }
     }
 
-    public static boolean checkSymbols(String s) {
-        if (s.length() > 20 || s.isEmpty() || s == null) {
+    private static boolean checkSymbols(String s) {
+
+        if (s.length() > 20) {
             return false;
         }
-        int count = 0;
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (Character.isAlphabetic(chars[i]) || Character.isDigit(chars[i])) {
-                count++;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!EXAMPLE.contains(String.valueOf(s.charAt(i)))) {
+                return false;
             }
         }
-        if (count == chars.length) {
-            return true;
-        } else return false;
-    }
+        return true;
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-import java.sql.SQLOutput;
+
 
 public class Main {
     public static void main(String[] args) {
-        Data first = new Data("21655156465165gfgjdf", "1254hfjd", "1254hfjd");
-        try {
-            System.out.println(Data.checkData(first.getLogin(), first.getPassword(), first.getConfirmPassword()));
-        } catch (WrongLoginException e) {
-            System.out.println("Неправильно введен логин");
-        } catch (WrongPasswordException e) {
-            System.out.println("Неправильно введен пароль");
+
+        boolean a =Data.check("gukg55","6546jhkg","6546jhkg");
+        if (a) {
+            System.out.println("Данные верны!");
+        } else {
+            System.out.println("Данные не верны!");
+
         }
+
     }
 }
